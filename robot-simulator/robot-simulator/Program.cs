c20f4dbb34cae814aa9687b2af66de04313689ca
Raymond Nowley <nowley.r@gmail.com>
2018-04-08 18:57:07 +0800

    Initial setup

---
+++ robot-simulator/robot-simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace robot_simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
